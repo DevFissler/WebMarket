3b5f81fb188ea04a2b15d58ad5e7e6d3d68f0dc4
DevFissler <101681361+DevFissler@users.noreply.github.com>
2022-06-09 13:07:51 +0900

    Update exam1.java

--- src/main/webapp/exam1.java
+++ src/main/webapp/exam1.java
@@ -1,37 +1,29 @@
 package freePrac;
 
 public class Word {
-	
+
 	private String letters;
 
 	public Word(String letters) {
 		this.letters = letters.toLowerCase();
 	}
-	
-	public boolean isVowel (int i) {
 
-		return (letters.substring(i, i+1).equals("a")||letters.substring(i, i+1).equals("e")||letters.substring(i, i+1).equals("i")||letters.substring(i, i+1).equals("o")||letters.substring(i, i+1).equals("u"));
-		
-	}
-	
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		Word word = new Word("Hello World");
-		System.out.println(word.isVowel2(0));
-		System.out.println(word.isVowel2(4));
-	
-	}
-	
-	public boolean isVowel2 (int i) {
-		String a = "aeiou";
-		boolean result=false;
-		for (int index=0;index<a.length();index++) {
-			if(letters.substring(i,i+1).equals(a.substring(index, index+1))==true) {
-				result=true;
-				break;
-				
-			}			
-		}
-		return result;
+		System.out.println(word.isConsonant(0));
+		System.out.println(word.isConsonant(2));
+
 	}
 
+	public boolean isConsonant(int i) {
+		String vowel = "aeiou";
+		boolean isCon = true;
+		for (int index = 0; index < vowel.length(); index++) {
+			if (letters.substring(i, i + 1).equals(vowel.substring(index, index + 1)) == true) {
+				isCon = false;
+				break;
+			}
+		}
+		return isCon;
+	}
 }
